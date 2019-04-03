47eb9b74d7c9d7da63648ec37f5a768621404b06
mvppoa <mvp.poa@gmail.com>
2019-04-03 15:35:50 +0100

    'test'

---
+++ src/main/java/com/mvppoa/jx/jxspringactuator/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.mvppoa.jx.jxspringactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
